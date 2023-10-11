2fee3d1e25a9638cad92ea1291bf3cdffc6fd29e
KISHORE011 <kishore011@gmail.com>
2023-10-11 11:30:46 -0800

    code

---
+++ qus01.py
@@ -0,0 +1,8 @@
+class Circle:
+    def __init__(self, data_list):
+        self.data = data_list
+
+data_list = [10, 501, 22, 37, 100, 999, 87, 351]
+circle = Circle(data_list)
+
+print(circle.data)
